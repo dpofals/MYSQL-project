647dc17a17457eb5fef3a67eb6553886010fd6de
OFURE FORTUNATE <124737057+dpofals@users.noreply.github.com>
2023-03-04 04:01:59 +0100

    Add files via upload

---
+++ SQL PROJECT.sql
@@ -0,0 +1,113 @@
+-- Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA
+SELECT * 
+FROM CITY
+WHERE POPULATION > 100000 AND COUNTRYCODE = "USA"
+
+-- Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
+SELECT DISTINCT CITY
+FROM STATION
+WHERE ID%2 = 0
+
+-- Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
+SELECT COUNT(CITY) - COUNT(DISTINCT CITY)
+FROM STATION
+
+-- Query the Name of any student in STUDENTS who scored higher than 75 Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+SELECT Name
+FROM STUDENTS
+WHERE Marks > 75
+ORDER BY RIGHT(Name, 3), ID ASC;
+
+-- Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+SELECT name
+FROM Employee
+ORDER BY name ASC;
+
+-- Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a salary greater than $2000 per month who have been employees for less than  months. Sort your result by ascending employee_id.
+SELECT name
+FROM Employee
+WHERE salary > 2000 
+AND months < "10"
+ORDER BY employee_id;
+
+-- Query the total population of all cities in CITY where District is California.
+SELECT SUM(POPULATION)AS total_pop
+FROM CITY
+WHERE DISTRICT = "California";
+
+-- 
+SELECT AVG(POPULATION)AS Query the average population of all cities in CITY where District is California.average_pop
+FROM CITY 
+WHERE DISTRICT = "California";
+
+-- Query the average population for all cities in CITY, rounded down to the nearest integer
+SELECT FLOOR(AVG(POPULATION)) 
+FROM CITY;
+
+-- Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCODE for Japan is JPN.
+SELECT SUM(POPULATION)
+FROM CITY
+WHERE COUNTRYCODE = "JPN";
+
+-- Query the difference between the maximum and minimum populations in CITY.
+SELECT MAX(POPULATION)- MIN(POPULATION)
+FROM CITY;
+
+-- Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY LIKE 'a%' OR CITY LIKE 'e%' OR CITY LIKE 'i%' OR CITY LIKE 'o%' OR CITY LIKE 'u%';
+
+-- Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+SELECT DISTINCT CITY
+FROM STATION
+WHERE RIGHT(CITY,1) IN  ('a', 'e', 'i', 'o', 'u'); 
+
+-- Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LEFT(CITY,1) IN ('a', 'e', 'i', 'o', 'u')
+AND RIGHT(CITY,1) IN ('a', 'e', 'i', 'o', 'u');
+
+-- Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LEFT(CITY,1) NOT IN ('a', 'e', 'i', 'o', 'u');
+
+-- Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY
+FROM STATION
+WHERE RIGHT(CITY,1) NOT IN ('a', 'e', 'i', 'o', 'u');
+
+-- Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LEFT(CITY,1) NOT IN ('a', 'e', 'i', 'o', 'u')
+OR RIGHT(CITY,1) NOT IN ('a', 'e', 'i', 'o', 'u');
+
+-- Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LEFT(CITY,1) NOT IN ('a', 'e', 'i', 'o', 'u')
+AND RIGHT(CITY,1) NOT IN ('a', 'e', 'i', 'o', 'u');
+
+-- Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.
+SELECT SUM(CITY.POPULATION)
+FROM CITY
+JOIN COUNTRY
+ON CITY.COUNTRYCODE= COUNTRY.CODE
+WHERE COUNTRY.CONTINENT = "Asia";
+
+-- Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
+SELECT CITY.NAME
+FROM CITY
+JOIN COUNTRY
+ON CITY.COUNTRYCODE = COUNTRY.CODE
+WHERE COUNTRY.CONTINENT = "Africa";
+
+-- Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population) rounded down to the nearest integer.
+SELECT COUNTRY.CONTINENT, FLOOR(AVG(CITY.POPULATION))
+FROM CITY
+JOIN COUNTRY
+ON CITY.COUNTRYCODE= COUNTRY.CODE
+GROUP BY COUNTRY.CONTINENT
